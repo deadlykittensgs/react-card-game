5eff82c4a9cd7d27b0070594e8200114cf1ab574
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-05-29 18:24:49 -0500

    uno game pivot

--- src/Components/PlayersHand.jsx
+++ src/Components/PlayersHand.jsx
@@ -6,9 +6,21 @@ import Card from './Card'
 export default function PlayersHand() {
   return (
     <section className='flex bg-green-400 content-center justify-center gap-3 p-8'>
+
+<div className='flex flex-col bg-blue-300'> 
+      <p>options</p>
+    <p>options</p>
+    <p>options</p>
+      </div>
       <Card/>
-        <Card/>
-        <Card/>
+      <Card/>
+      <Card/>
+
+        <div className='flex flex-col bg-red-400'>
+<p>options</p>
+<p>options</p>
+<p>options</p>
+        </div>
     </section>
   )
 }
